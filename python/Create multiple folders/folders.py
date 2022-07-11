af6dab2304752849956d5bc1261e9606ed35ee20
maulanaakbardj <asinmanis11@gmail@.com>
2022-07-11 08:20:08 +0700

    Add Path folders.py

--- python/Create multiple folders/folders.py
+++ python/Create multiple folders/folders.py
@@ -8,4 +8,4 @@ days = ['Day 1', 'Day 10', 'Day 20', 'Day 30']
 
 for i, month in enumerate(month_names):
     for day in days:
-        Path(f'python/2022/{i+1}.{month}/{day}').mkdir(parents = True, exist_ok = True)
+        Path(f'python/Create multiple folders/results/2022/{i+1}.{month}/{day}').mkdir(parents = True, exist_ok = True)
