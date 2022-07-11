cd0ec673be2cd1768796600dfa78ac7e02d0f849
maulanaakbardj <asinmanis11@gmail@.com>
2022-07-11 08:16:27 +0700

    Update foders.py

--- python/folders.py
+++ python/folders.py
@@ -1,9 +1,11 @@
 import calendar
 from pathlib import Path
 
-from webob import day
+from webob import day, month
 
 month_names = list(calendar.month_name[1:])
 days = ['Day 1', 'Day 10', 'Day 20', 'Day 30']
 
-Path('2022/January').mkdir(parents = True, exist_ok = True)
+for i, month in enumerate(month_names):
+    for day in days:
+        Path(f'2022/{i+1}.{month}/{day}').mkdir(parents = True, exist_ok = True)
